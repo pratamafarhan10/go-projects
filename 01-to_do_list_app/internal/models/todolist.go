5097d8da7410491fe41d44ca344b7a5f675f5438
pratamafarhan10 <pratamafarhan10@gmail.com>
2022-10-14 20:49:41 +0700

    todolist query all done

--- 01-to_do_list_app/internal/models/todolist.go
+++ 01-to_do_list_app/internal/models/todolist.go
@@ -37,28 +37,38 @@ type TodoLists struct {
 }
 
 func (t TodoList) GetTodoList(filter bson.M, projection bson.M, dst any) error {
-	res := TodoListCollection.FindOne(context.Background(), filter, options.FindOne().SetProjection(projection))
+	res := TodoListCollection.FindOne(context.TODO(), filter, options.FindOne().SetProjection(projection))
 	err := res.Decode(dst)
 
 	return err
 }
 
+func (t TodoList) GetTodoLists(filter bson.M, dst TodoLists) error {
+	cur, err := TodoListCollection.Find(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+
+	err = cur.All(context.TODO(), &dst)
+	return err
+}
+
 func (t TodoList) InsertTodoList() (string, error) {
 	// Check if the data has been inserted before
-	n, err := TodoListCollection.CountDocuments(context.Background(), bson.M{"date": t.Date, "user_id": t.User_Id})
+	n, err := TodoListCollection.CountDocuments(context.TODO(), bson.M{"date": t.Date, "user_id": t.User_Id})
 	if err != nil {
 		return "", err
 	}
 
 	if n < 1 {
-		res, err := TodoListCollection.InsertOne(context.Background(), t)
+		res, err := TodoListCollection.InsertOne(context.TODO(), t)
 
 		oid, _ := res.InsertedID.(primitive.ObjectID)
 		return oid.Hex(), err
 	}
 
 	for _, val := range t.Todos {
-		_, err = TodoListCollection.UpdateOne(context.Background(), bson.M{"date": t.Date, "user_id": t.User_Id}, bson.M{"$push": val})
+		_, err = TodoListCollection.UpdateOne(context.TODO(), bson.M{"date": t.Date, "user_id": t.User_Id}, bson.M{"$push": val})
 		if err != nil {
 			return "", err
 		}
@@ -69,7 +79,7 @@ func (t TodoList) InsertTodoList() (string, error) {
 
 func (t TodoList) UpdateTodoList(data UpdateTodosRequest) error {
 	_, err := TodoListCollection.UpdateOne(
-		context.Background(),
+		context.TODO(),
 		bson.M{"date": data.Date, "user_id": data.User_Id, "todos": bson.M{"$elemMatch": bson.M{"_id": data.TodoId}}},
 		bson.M{"$set": bson.M{"todos.$.title": data.Title, "todos.$.completed": data.Completed, "todos.$.time": data.Time}},
 	)
